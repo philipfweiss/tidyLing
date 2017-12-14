3676caf4620c0ed1af49f8976677ce31af02ea07
philipfweiss <weissp68@stanford.edu>
2017-12-14 11:58:06 -0800

    add basic ingestor

--- lib/ingestor.py
+++ lib/ingestor.py
@@ -1,4 +1,5 @@
 import os
+from constants import supported_filetypes
 
 class Ingestor:
     def __init__(self):
@@ -7,19 +8,21 @@ class Ingestor:
     def ingest(self, filepath):
         try:
             file = open(filepath, 'r')
+            name, ext = os.path.splitext(filepath)
+            ext = ext[1:]
+            if ext in supported_filetypes:
+                self.invoke_parser(file, ext)
+
         except Exception:
             print("Unable to parse " + filepath)
 
-        name, ext = os.path.splitext(filepath)
-        ext = ext[1:]
 
-
-    def invoke_parser(self):
-        pass
+    def invoke_parser(self, file, ext):
+        print("parser invoked!")
 
 
 ing = Ingestor()
-ing.ingest("foo.txt")
+ing.ingest("foo.tex")
 
 
     
